46d96ca93eab3a428f2cb5597f1bdd33331b4489
James Casey <waferthinninja@gmail.com>
2017-07-23 11:31:19 +0100

    Add head that follows mouse, tongue that catches flies

---
+++ Assets/Scripts/FlyCatcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyCatcher : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Fly")
+        {
+            // kill the fly and stick it to the tongue
+            FlyController fc = other.gameObject.GetComponent<FlyController>();
+            fc.Die();
+            fc.transform.SetParent(this.transform);
+        }
+
+    }
+}
