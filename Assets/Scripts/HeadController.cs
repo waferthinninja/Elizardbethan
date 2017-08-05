56cae1180f8286c812f1517fe0298467cb3f8eee
James Casey <waferthinninja@gmail.com>
2017-08-05 19:02:26 +0100

    WIP (transfer from laptop to PC:) )

--- Assets/Scripts/HeadController.cs
+++ Assets/Scripts/HeadController.cs
@@ -6,13 +6,14 @@ public class HeadController : MonoBehaviour
 {
     public Transform DeadFlyParent; // this will be the collider object that we attach flies to when they are caught
     public Collider2D TongueCollider;
-    public float MinAngle = -45; // furthest the head can rotate, in degrees
-    public float MaxAngle = 45; // furthest the head can rotate, in degrees
+    public float MinAngle = -25; // furthest the head can rotate, in degrees
+    public float MaxAngle = 65; // furthest the head can rotate, in degrees
     public float RotationSpeed = 200; // Only relevant in "axis" aiming mode
 
     private Animator _animator;
     public bool IsFiring { get; private set; }
 
+    private float _rotation = 0;
     private bool _mouseAiming = false;
 
     // Use this for initialization
@@ -35,10 +36,20 @@ public class HeadController : MonoBehaviour
             {
                 // key input
                 float yAxis = Input.GetAxis("Vertical");
-                float angle = transform.eulerAngles.z + (yAxis * RotationSpeed * Time.deltaTime);
-                if (angle > 90) angle -= 360; // turn angle like 345 to be -15                
-                angle = Mathf.Clamp(angle, MinAngle, MaxAngle);
-                transform.rotation = Quaternion.Euler(0, 0, angle);
+                float turnAmount = yAxis * RotationSpeed * Time.deltaTime;
+                _rotation += turnAmount;
+                                
+                if (_rotation < MinAngle)
+                {
+                    turnAmount -= _rotation - MinAngle;
+                    _rotation = MinAngle;
+                }
+                if (_rotation > MaxAngle)
+                {
+                    turnAmount -= _rotation - MaxAngle;
+                    _rotation = MaxAngle;
+                }
+                transform.RotateAround(transform.position, transform.right, turnAmount);
             }
 
             if (Input.GetButton("Fire1"))
@@ -61,7 +72,7 @@ public class HeadController : MonoBehaviour
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg;
         angle = Mathf.Clamp(angle, MinAngle, MaxAngle);
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        transform.rotation = Quaternion.Euler(angle - 45, -90, 0);
     }
 
 
